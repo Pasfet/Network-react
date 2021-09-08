7225e1ca7d53aa7539759f7f8a043cecfce9f3cf
Pavel <pav.konstantinov2016@yandex.ru>
2021-09-08 20:47:42 +0300

    first-settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Message from "./components/Message/Message";
 
-function App() {
+const App = () => {
+  const textMsg = 'This is text from props';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message message={textMsg} />
     </div>
   );
 }
---
+++ src/components/Message/Message.js
@@ -0,0 +1,9 @@
+const Message = (props) => {
+  return (
+    <div className="message">
+      {props.message}
+    </div>
+  );
+};
+
+export default Message;
