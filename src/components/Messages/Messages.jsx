5e978843532fa163f41e60ed9074b0d2097af669
Pavel <pav.konstantinov2016@yandex.ru>
2021-09-23 15:11:17 +0300

    fix

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import style from './Messages.module.scss';
 
 const Messages = ({ messagesList }) => {
-  const msg = messagesList.map((message) => 
-    <Message message={message} key={message.id} />
-  );
   return (
     <div className={style.messagesWrapper}>
-      {msg}
+      {messagesList?.map((message) => 
+        <Message message={message} key={message.id} />
+      )}
     </div>
   );
 };
